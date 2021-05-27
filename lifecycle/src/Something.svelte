7c054b3cb371050927754b687870ebbb447e209a
Hyeonproject <axzswq@gmail.com>
2021-05-28 01:03:15 +0900

    onDesotry and reuturn and async error learn

--- lifecycle/src/Something.svelte
+++ lifecycle/src/Something.svelte
@@ -3,12 +3,15 @@
 
   onMount(() => {
     console.log("onMount");
-    const h1 = document.querySelector("h1");
-    console.log(h1.innerText);
+    return () => {
+      //이슈가 많아서 이는 사라진 문법이다. 비동기 시에서는 promise 객체로 반환되기
+      //때문에 사용이 힘들다.
+      console.log("Destory in mount");
+    };
   });
 
   onDestroy(() => {
-    console.log("Destory");
+    console.log("Before Destory");
   });
 </script>
 
