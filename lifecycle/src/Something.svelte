e38695db7cb7f4853c60056d0457d2f6d7641827
Hyeonproject <axzswq@gmail.com>
2021-05-28 00:59:11 +0900

    learning onmount

--- lifecycle/src/Something.svelte
+++ lifecycle/src/Something.svelte
@@ -1,1 +1,15 @@
-<div>Something</div>
+<script>
+  import { onMount, onDestroy } from "svelte";
+
+  onMount(() => {
+    console.log("onMount");
+    const h1 = document.querySelector("h1");
+    console.log(h1.innerText);
+  });
+
+  onDestroy(() => {
+    console.log("Destory");
+  });
+</script>
+
+<h1>Something..</h1>
