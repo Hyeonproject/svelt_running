65de64aac5859aef7ca9b5b0589d7e818da8b72c
Hyeonproject <axzswq@gmail.com>
2021-06-02 01:53:43 +0900

    learn svelte !!

---
+++ tick/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { tick } from "svelte";
+  let name = "world";
+
+  async function handler() {
+    name = "Hyeonproject";
+    await tick();
+    const h1 = document.querySelector("h1");
+    console.log(h1.innerText);
+  }
+</script>
+
+<h1 on:click={handler}>
+  Hello {name}!
+</h1>
