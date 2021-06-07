d85856ddfc193205d3d1cf32032db75fb81ddf9a
Hyeonproject <axzswq@gmail.com>
2021-06-08 01:52:38 +0900

    all learning

--- learn10/src/App.svelte
+++ learn10/src/App.svelte
@@ -1,6 +1,16 @@
 <script>
   let h1 = "<h1>Hello Hyeonproject</h1>";
   let xss = '<iframe onload="document.location=``)"></iframe>';
+
+  let name = "Hyeonproject";
+  let index = 0;
 </script>
 
-{@html h1}
+{@debug index, name}
+<h1
+  on:click={() => {
+    index += 1;
+  }}
+>
+  Hello {name}!
+</h1>
