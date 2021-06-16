8885769a58941757915484c6ada6dc4f97ed42c4
Hyeonproject <axzswq@gmail.com>
2021-06-17 01:31:33 +0900

    assignment learn

---
+++ four/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { assign } from "svelte/internal";
+
+  let count = 0;
+  let double = 0;
+
+  $: {
+    double = count * 2;
+  }
+
+  function assigns() {
+    count += 1;
+  }
+</script>
+
+<button on:click={assigns}>Assign</button>
+<h2>{count}</h2>
+<h2>{double}</h2>
