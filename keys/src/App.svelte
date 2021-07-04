dcac2d638bc02beeaf723be10825faafdd4ad4cd
Hyeonproject <axzswq@gmail.com>
2021-07-04 15:03:53 +0900

    learm  kesy

---
+++ keys/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let fruits = ["Apple", "Banana", "Cherry", "Orange"];
+
+  function deleteFrist() {
+    fruits = fruits.slice(1);
+  }
+</script>
+
+<button on:click={deleteFirst}> Delete frist fruit! </button>
+
+<ul>
+  {#each fruits as fruit (fruit)}
+    <li>{fruit}</li>
+  {/each}
+</ul>
