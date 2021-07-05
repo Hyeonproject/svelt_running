e044763ef924a347032128575ae8f941412ba936
Hyeonproject <axzswq@gmail.com>
2021-07-05 23:19:48 +0900

    learn svelt key

--- keys/src/App.svelte
+++ keys/src/App.svelte
@@ -1,5 +1,11 @@
 <script>
-  let fruits = ["Apple", "Banana", "Cherry", "Orange"];
+  // let fruits = ["Apple", "Banana", "Cherry", "Orange"];
+  let fruits = [
+    { id: "1", name: "Apple" },
+    { id: "2", name: "Cherry" },
+    { id: "3", name: "Banana" },
+    { id: "4", name: "Apple" },
+  ];
 
   function deleteFrist() {
     fruits = fruits.slice(1);
@@ -9,7 +15,7 @@
 <button on:click={deleteFirst}> Delete frist fruit! </button>
 
 <ul>
-  {#each fruits as fruit (fruit)}
+  {#each fruits as fruit (fruit.id)}
     <li>{fruit}</li>
   {/each}
 </ul>
